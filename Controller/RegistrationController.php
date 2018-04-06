38bc6f516a718b84c0b3e64c129afbc1d4bbddf9
developer <dima3141592@gmail.com>
2018-04-06 18:53:46 +0300

    fix

--- Controller/RegistrationController.php
+++ Controller/RegistrationController.php
@@ -6,5 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class RegistrationController extends Controller
 {
+    public function regForm()
+    {
+        return $this->render('@PaladinUser/Registration/regForm.html.twig');
+    }
 
+    public function registration()
+    {
+        return $this->render('@PaladinUser/Registration/regForm.html.twig');
+    }
 }
